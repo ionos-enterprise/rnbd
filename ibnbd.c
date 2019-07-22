7113bce6981d994af233115b584b65c7a28a2ddf
Danil Kipnis <danil.kipnis@cloud.ionos.com>
2019-07-22 08:50:42 +0200

    ibnbd: make MP short the default field for list of sessions
    Signed-off-by: Danil Kipnis <danil.kipnis@cloud.ionos.com>

--- ibnbd.c
+++ ibnbd.c
@@ -536,7 +536,7 @@ static struct table_column *def_clms_sessions_clt[] = {
 	&clm_ibnbd_sess_sessname,
 	&clm_ibnbd_sess_state,
 	&clm_ibnbd_sess_path_uu,
-	&clm_ibnbd_sess_mp,
+	&clm_ibnbd_sess_mp_short,
 	&clm_ibnbd_sess_tx_bytes,
 	&clm_ibnbd_sess_rx_bytes,
 	&clm_ibnbd_sess_inflights,
