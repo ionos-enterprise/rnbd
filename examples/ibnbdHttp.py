ade096ec1eb83dc16d6da24536b4bbe41182ba08
Danil Kipnis <danil.kipnis@cloud.ionos.com>
2020-02-17 12:17:44 +0100

    examples: fixup Http server
    Signed-off-by: Danil Kipnis <danil.kipnis@cloud.ionos.com>

--- examples/ibnbdHttp.py
+++ examples/ibnbdHttp.py
@@ -15,7 +15,7 @@ Usage:
 class RequestHandler(SimpleHTTPRequestHandler):
 
     def do_GET(self):
-        command = "rbnbd dump json all"
+        command = "rnbd dump json all"
         proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=None,
                                 shell=True)
         jsonstr = proc.communicate()[0]
