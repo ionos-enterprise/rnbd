e247fbd4b8d21232870a2a676296c19823a762b8
Danil Kipnis <danil.kipnis@ionos.com>
2021-06-29 09:02:59 +0200

    scripts: add release.sh script
    Do add the release relevant commits for do
    
    ./release.sh <new version>
    
    Signed-off-by: Danil Kipnis <danil.kipnis@ionos.com>

---
+++ release.sh
@@ -0,0 +1,37 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+#!/bin/bash
+set -e
+
+version="$1"
+
+if [ -z "$version" ]; then
+	echo "Please provide the version to be released";
+	exit
+else
+	echo "Releasing version $version"
+fi
+
+# Change version in the Makefile to the new $version
+sed -i "s/^VERSION.*/VERSION := $version/" Makefile
+
+# Update the man page
+make man/rnbd.8
+
+# Commit the changes
+git commit -asm "rnbd: release $version"
+
+# Tag the version of the source code package
+git tag $version
+
+# Add new debian/changelog entry
+gbp dch --new-version=$version --git-author --ignore-branch --distribution="jessie stretch buster bullseye" --force-distribution
+
+# Commit the change
+git commit -asm "deb: release rnbd-$version"
+
+# Tag the debian release
+git tag rnbd-$version
+
+# Push
+git log -2
+echo "Now please do \$git push --tags"
