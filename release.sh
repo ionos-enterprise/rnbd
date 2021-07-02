2b6440e8ba3289b3538f0ca5181a5773e7965061
Danil Kipnis <danil.kipnis@ionos.com>
2021-07-02 09:01:53 +0200

    scripts: remove manpage md before generating a new one
    Signed-off-by: Danil Kipnis <danil.kipnis@ionos.com>

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 #!/bin/bash
-set -e
+set -ex
 
 version="$1"
 
@@ -15,6 +15,8 @@ fi
 sed -i "s/^VERSION.*/VERSION := $version/" Makefile
 
 # Update the man page
+rm rnbd.8.md
+rm man/rnbd.8
 make man/rnbd.8
 
 # Commit the changes
